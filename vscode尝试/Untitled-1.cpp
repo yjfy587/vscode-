ce41211b3ec934a54cb0c31dee2696de3d4e58a7
yjfy <25189100091@stu.xidian.edu.cn>
2026-03-05 21:34:51 +0800

    feat：对树进行了补充

--- vscode尝试/Untitled-1.cpp
+++ vscode尝试/Untitled-1.cpp
@@ -1,6 +1,28 @@
 #include<iostream>
 using namespace std;
+struct node{
+    int data;
+    node *lchild=NULL;
+    node *rchild=NULL;
+}node[100];
+void pushtree(node *root){
+    cout<<root->data<<endl;
+    pushtree(root->lchild);
+    pushtree(root->rchild);
+}
 int main(){
-
-    cout<<"hello world"<<endl;
+    int n;
+    cin>>n;
+    int a,b,c;
+    for(int i=0;i<n;i++){
+        cin>>a>>b>>c;
+        node[i].data=c;
+        if(a!=-1){
+            node[i].lchild=&node[a];
+        }
+        if(b!=-1){
+            node[i].rchild=*node[b];
+        }
+    }
+    return 0;
 }
